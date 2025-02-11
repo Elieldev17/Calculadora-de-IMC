2913cb9e3e7b442ac8ac73c9acca69c2f1f71b7f
ElielDev17 <elielsh32@gmail.com>
2025-02-11 15:33:48 -0300

    finalizado o PWA

---
+++ Models/ImcModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Imc.Models;
+
+public class ImcModel
+{
+    [Required(ErrorMessage = "Informe a altura")]
+    [Range(1, 3, ErrorMessage = "altura invalida")]
+    public double? Heigth { get; set; } //altura
+    
+    [Required(ErrorMessage = "Informe o peso")]
+    [Range(15, 180, ErrorMessage = "peso invalido")]
+    public double? Weigth { get; set; } //peso
+
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public double Imc => (Weigth ?? 1) / ((Heigth ?? 1) * (Heigth ?? 1));
+
+}
